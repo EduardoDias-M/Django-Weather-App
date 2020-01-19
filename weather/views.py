aa37cffda3a00fddab12ac06818586b3b02017cb
Eduardo Dias <eduardodiasprog@gmail.com>
2020-01-19 16:38:00 -0300

    showing the datas from db and creating the forms.py

--- weather/views.py
+++ weather/views.py
@@ -1,21 +1,27 @@
-from django.shortcuts import render
 import requests
+from django.shortcuts import render
+from .models import City
 
 # Create your views here.
 
 def index(request):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=yourkey'
-    city = 'Las Vegas'
-    
-    req = requests.get(url.format(city)).json()
+    city = 'London'
 
-    city_weather = {
-        'city' : city,
-        'temperature' : req['main']['temp'],
-        'description' : req['weather'][0]['description'],
-        'icon' : req['weather'][0]['icon'],
-    }
+    cities = City.objects.all()
+    weather_data = []
 
-    context = {'city_weather' : city_weather}
+    for city in cities:
+        req = requests.get(url.format(city)).json()
+
+        city_weather = {
+            'city' : city.name,
+            'temperature' : req['main']['temp'],
+            'description' : req['weather'][0]['description'],
+            'icon' : req['weather'][0]['icon'],
+        }
+        weather_data.append(city_weather)
+        
+    context = {'weather_data' : weather_data}
     
     return render(request, 'index.html', context)
