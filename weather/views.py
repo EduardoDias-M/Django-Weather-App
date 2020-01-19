649b03039b2caa434cc190b81363a9c45b0769cf
Eduardo Dias <eduardodiasprog@gmail.com>
2020-01-19 16:00:43 -0300

    showing the api datas in las vegas city

--- weather/views.py
+++ weather/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+import requests
 
 # Create your views here.
+
+def index(request):
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid=yourkey'
+    city = 'Las Vegas'
+    
+    req = requests.get(url.format(city)).json()
+
+    city_weather = {
+        'city' : city,
+        'temperature' : req['main']['temp'],
+        'description' : req['weather'][0]['description'],
+        'icon' : req['weather'][0]['icon'],
+    }
+
+    context = {'city_weather' : city_weather}
+    
+    return render(request, 'index.html', context)
